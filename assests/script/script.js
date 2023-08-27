050528d6c94f0cf70e8bc1ac56e9199682990d91
sefufim <132181028+sefufim@users.noreply.github.com>
2023-08-27 14:04:01 -0700

    added necessary documents and made api call, also added the header

---
+++ assests/script/script.js
@@ -0,0 +1,6 @@
+// var key = "a6b192a96b840ed8a0ce76098bebce52";
+// var longitude;
+// var latitude;
+// var url = "http://api.openweathermap.org/data/2.5/forecast?lat=" + longitude + "&lon=" + latitude +  "&appid=" + key;
+
+// fetch(url)
