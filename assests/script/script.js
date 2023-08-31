a6eeb87c3c7487c05378a86e795c3b8a7b673659
sefufim <132181028+sefufim@users.noreply.github.com>
2023-08-31 14:21:14 -0700

    added the current weather function and the even listener

--- assests/script/script.js
+++ assests/script/script.js
@@ -1,6 +1,26 @@
-// var key = "a6b192a96b840ed8a0ce76098bebce52";
-// var longitude;
-// var latitude;
-// var url = "http://api.openweathermap.org/data/2.5/forecast?lat=" + longitude + "&lon=" + latitude +  "&appid=" + key;
+$(document).ready(function(){
+    // This function listens for when the search button is pressed then it shows then it calls the current weather funtion
+    $('.search').on('click', function (event) {
+        event.preventDefault();
+        var cityInput = $(".search-input").val();
+        currentWeather(cityInput);
+        
+    });
+    
+    //the current weather function pulls in the city input and search for it then displays it on the console log 
+    var currentWeather = function(cityInput) {
+        var key = "a6b192a96b840ed8a0ce76098bebce52";
+        $.ajax({
+            type: "GET",
+            url: `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${key}`
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error("Error fetching weather data:", error);
+        });
+    };
+    
+})
 
-// fetch(url)
