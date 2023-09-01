d90e0ee3cacb2b0c8c94a16cff64014385d6f150
sefufim <132181028+sefufim@users.noreply.github.com>
2023-08-31 17:32:01 -0700

    added styling to history and reworked html to use bootstrap

--- assests/script/script.js
+++ assests/script/script.js
@@ -1,42 +1,53 @@
-$(document).ready(function(){
-    // This function listens for when the search button is pressed then it calls the current weather function
-    $('.search').on('click', function (event) {
-        event.preventDefault();
-        var cityInput = $(".search-input").val();
-        currentWeather(cityInput);
-        storeHistory(cityInput);
-    });
 
-    // Stores user history
-    var storeHistory = function (cityInput){
-        var history = JSON.parse(localStorage.getItem("history")) || [];
-        history.push(cityInput);
-        localStorage.setItem("history", JSON.stringify(history));
-        displayHistory();
+$(document).ready(function () {
+    //  this function retrieves the stored history from local storage
+    var getStoredHistory = function () {
+      return JSON.parse(localStorage.getItem("history")) || [];
     };
+  
+    // this function stores history in local storage
+    var storeHistory = function (cityInput) {
+      var history = getStoredHistory();
+      history.push(cityInput);
+      localStorage.setItem("history", JSON.stringify(history));
+    };
+  
+    // this function displays history
+    var displayHistory = function () {
+      var storedHistory = getStoredHistory();
+      var historyList = $(".history");
+      for (var i = 0; i < storedHistory.length; i++) {
+        var listItem = $("<li>").addClass("list-group-item btn text-black-50 mb-3 ").text(storedHistory[i]);
+        historyList.append(listItem);
 
-    // Displays history on the main page
-    var displayHistory = function() {
-       var storedHistory = JSON.parse(localStorage.getItem("history"));
-       for (var i = 0; i < storedHistory.length; i++) {
-        $(".history").append(`<li>${storedHistory[i]}</li>`);
-       }
-       console.log(storedHistory);
     }
-    //the current weather function pulls in the city input and search for it then displays it on the console log 
-    var currentWeather = function(cityInput) {
-        var key = "a6b192a96b840ed8a0ce76098bebce52";
-        $.ajax({
-            type: "GET",
-            url: `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${key}`
+};
+    
+  
+    //this function displays current weather
+    var currentWeather = function (cityInput) {
+      var key = "a6b192a96b840ed8a0ce76098bebce52";
+      $.ajax({
+        type: "GET",
+        url: `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${key}`,
+        dataType: "json",
+      })
+        .then((data) => {
+          console.log(data);
         })
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error("Error fetching weather data:", error);
+        .catch((error) => {
+          console.error("Error fetching weather data:", error);
         });
     };
-    
-});
-
+  
+    // Search button click event
+    $(".search").on("click", function (event) {
+      event.preventDefault();
+      var cityInput = $(".search-input").val();
+      currentWeather(cityInput);
+      storeHistory(cityInput);
+      displayHistory();
+    });
+  
+  });
+  
